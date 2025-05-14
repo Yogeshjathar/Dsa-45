7b4493c4151807d9221c0c1382b4b3000356af08
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2025-05-14 13:37:56 +0530

    Solution

---
+++ Armstrong.java
@@ -0,0 +1,27 @@
+public class Armstrong {
+
+
+    public static boolean  isArmstrong(int num){
+        int originalNum = num;
+        int power = String.valueOf(num).length();
+        int sum = 0;
+
+
+        while(num > 0){
+            int singleDigit = num % 10;
+            sum += Math.pow(singleDigit, power);
+            num = num / 10;
+        }
+
+
+        return originalNum == sum;
+    }
+
+
+    public static void main(String[] args) {
+        int num = 153;     // 153, 9474
+        System.out.println(isArmstrong(num));
+
+
+    }
+}

823fede05e3c597ac71465cd2ee0a89063a77a3a
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2025-07-03 16:01:40 +0530

    Two sum problem using two approach- nested Loop and HashMap

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,4 @@
+package Other;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -5,36 +6,44 @@ import java.util.HashMap;
 public class TwoSum {
     public static int[] getTwoSumIndex_BruteForce(int arr[], int sum){
 
-        for(int i=0; i<arr.length-2; i++){
-            for(int j=0; j<arr.length-1; j++){
+        // Outer loop
+        for(int i=0; i<arr.length-1; i++){
+            // nested loop
+            for(int j=i+1; j<arr.length; j++){
+                // check sum and return
                 if(arr[i]+arr[j] == sum){
                     return new int[]{i,j};
                 }
             }
         }
 
-
+        // return -1,-1 if not exist
         return new int[]{-1,-1};
     }
 
     public static int[] getTwoSumIndex_Optimized(int arr[], int sum){
 
         HashMap<Integer, Integer> hm = new HashMap<>();
+
+        // Iterate
         for(int i=0; i<arr.length; i++){
             int complement = sum - arr[i];
+
+            // check in hashmap if complement is there
             if(hm.containsKey(complement)){
                 return new int[]{hm.get(complement),i};
             }
+            // If complement is not in hm then put the elements
             hm.put(arr[i], i);
         }
 
-
+        // return -1,-1 if not exist
         return new int[]{-1,-1};
     }
     
     public static void main(String[] args) {
         int arr[] = {1,24,3,6,765,64};
-        int sum = 30;
+        int sum = 65;
 
         // Time Complexity - O(n^2)
         System.out.println("BruteForce: "+Arrays.toString(getTwoSumIndex_BruteForce(arr, sum)));

71235a631a6418e16c4f19a226b381e09b2079e1
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2025-07-09 17:36:27 +0530

    Kth Smallest element using 4 approaches

--- KthSmallestElement.java
+++ KthSmallestElement.java
@@ -10,7 +10,8 @@ public class KthSmallestElement{
         int n2 = num2.length;
         System.out.println("BruteForec: "+getKthSmallest_BruteForec(num1, num2, n1, n2, k));
         System.out.println("Better: "+getKthSmallest_Better(num1, num2, n1, n2, k));
-        System.out.println("Optimized: "+getKthSmallest_Optimized(num1, num2, n1, n2, k));
+        System.out.println("Optimized: "+getKthSmallest_Optimized1(num1, num2, n1, n2, k));
+        System.out.println("Optimized: "+getKthSmallest_Optimized2(num1, num2, n1, n2, k));
     }
 
     // Time Complexity: O(n1+n2) * log(n1+n2)
@@ -67,7 +68,7 @@ public class KthSmallestElement{
 
     // Time Complexity: O(n1+n2)
     // Space Complexity: O(1)
-    public static int getKthSmallest_Optimized(int num1[],int num2[], int n1, int n2,int k){
+    public static int getKthSmallest_Optimized1(int num1[],int num2[], int n1, int n2,int k){
         if(k < 0 || k > n1+n2) return -1;
         
         int i = 0;
@@ -98,6 +99,36 @@ public class KthSmallestElement{
             count++;
             if(count == k) return num2[j];
             j++;
+        }
+
+        return -1;
+    }
+
+    // Time Complexity: O(log(min(n1,n2))
+    // Space Complexity: O(1)
+    public static int getKthSmallest_Optimized2(int num1[],int num2[], int n1, int n2,int k){
+
+        if(n1 > n2) return getKthSmallest_Optimized2(num2, num1, n2, n1, k); // Take a smaller array
+
+        int low = Math.max(0, k-n2);
+        int high = Math.min(k, n1);
+
+        while(low <= high){
+            int cut1 = low + (high - low) / 2;
+            int cut2 = k - cut1;
+
+            int l1 = (cut1==0) ? Integer.MIN_VALUE : num1[cut1-1];
+            int l2 = (cut2==0) ? Integer.MIN_VALUE : num2[cut2-1];
+            int r1 = (cut1 == n1) ? Integer.MAX_VALUE : num1[cut1];
+            int r2 = (cut2 == n2) ? Integer.MAX_VALUE : num2[cut2];
+
+            if(l1 <= r2 && l2 <= r1){
+                return Math.max(l1,l2);
+            }else if(l1 > r2){
+                high = cut1 - 1;
+            }else{
+                low = cut1 + 1;
+            }
         }
 
         return -1;
